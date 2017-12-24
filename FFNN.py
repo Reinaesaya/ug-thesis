5724cfb36edb3e8dfa9f3cb8914d1ee4a8a0942d
Reinaesaya <xseanhylx@gmail.com>
2017-12-24 17:42:00 +0900

    Added compensator

--- FFNN.py
+++ FFNN.py
@@ -9,20 +9,28 @@ import matplotlib.pyplot as plt
 import random
 import os
 
-class FFNN_Regression():
-	def __init__(self, hidden_layers, num_inputs, num_outputs):
+class FFNN_1():
+	"""
+	Feed forward neural network instantiation with
+	tanh activation each layer except for output layer
+	"""
+	def __init__(self, hidden_layers, num_inputs, num_outputs, model=None):
 		if len(hidden_layers) < 1:
 			print ('No hidden layers???')
 
 		self.hidden_layers = hidden_layers
 		self.num_inputs = num_inputs
 		self.num_outputs = num_outputs
+		self.model = model
+		self.sessopen = False
 
 		self.init_ioplaceholders()
 		self.init_weights()
 		self.init_biases()
 		self.construct_model()
+		self.init_optimizer()
 
+# Model constructions
 	def init_ioplaceholders(self):
 		self.X = tf.placeholder("float", [None, self.num_inputs])
 		self.Y = tf.placeholder("float", [None, self.num_outputs])
@@ -43,96 +51,97 @@ class FFNN_Regression():
 
 	def construct_model(self):
 		self.layers = []
-		self.layers.append(tf.add(tf.matmul(self.X, self.weights['1']), self.biases['1']))
+		self.layers.append(tf.tanh(tf.add(tf.matmul(self.X, self.weights['1']), self.biases['1'])))
 		for i in range(len(self.hidden_layers)-1):
-			self.layers.append(tf.add(tf.matmul(self.layers[-1], \
-				self.weights[str(i+2)]), self.biases[str(i+2)]))
+			self.layers.append(tf.tanh(tf.add(tf.matmul(self.layers[-1], \
+				self.weights[str(i+2)]), self.biases[str(i+2)])))
 		self.layers.append(tf.add(tf.matmul(self.layers[-1],\
 			self.weights['out']), self.biases['out']))
 
 		self.output = self.layers[-1]
+		self.loss_op = tf.reduce_mean(tf.square(self.Y - self.output))
 
+	def init_optimizer(self):
+		self.learning_rate_placeholder = tf.placeholder(tf.float32, [], name='learning_rate')
+		self.optimizer = tf.train.AdamOptimizer(\
+			learning_rate=self.learning_rate_placeholder).minimize(self.loss_op)
 
-	def init_sess(self):
-		self.gv_init = tf.global_variables_initializer()
-		self.saver = tf.train.Saver()
+# Session open and close
+	def openSession(self):
+		if self.sessopen:
+			print('Session already open')
+		else:
+			self.gv_init = tf.global_variables_initializer()
+			self.saver = tf.train.Saver()
+			self.sess = tf.Session()
 
+			if (self.model == None) or not os.path.exists(self.model+'.index'):
+				self.sess.run(self.gv_init)
+			else:
+				self.saver.restore(self.sess, self.model)
+				print('Model %s restored' % self.model)
+			self.sessopen = True
+			print('Session opened')
+
+	def closeSession(self):
+		if self.sessopen:
+			self.sess.close()
+			self.sessopen = False
+			print('Session closed')
+		else:
+			print('Session already closed')
+
+	def saveNN(self):
+		self.save_path = self.saver.save(self.sess, self.model)
+		print("Model saved in file: %s" % self.save_path)
 
 # For training
-	def init_optimizer(self, learning_rate=0.001):
-		self.loss_op = tf.reduce_mean(tf.square(self.Y - self.output))
-		self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss_op)
 
-	def train(self, dataX, dataY, num_steps=5000, batch_size=128, display_step=100, model=None):
+	def train(self, dataX, dataY, learning_rate=0.001,\
+		num_steps=100000, batch_size=128, display_step=100):
 		# Initialize the variables (i.e. assign their default value)
 		data_inds = [i for i in range(len(dataX))]
 
-		self.init_sess()
-
 		# Start training
-		with tf.Session() as sess:
-			# Run the initializer
-			if (model == None) or not os.path.exists(model):
-				sess.run(self.gv_init)
+		batch_start = 0
+		batch_end = batch_size
+
+		for step in range(1, num_steps+1):
+			batch_inds = data_inds[batch_start:batch_end]
+			batch_x = [dataX[i] for i in batch_inds]
+			batch_y = [dataY[i] for i in batch_inds]
+
+			# Increment next batch
+			if batch_end < len(dataX):
+				batch_start += batch_size
+				batch_end += batch_size
+				batch_end = min(batch_end,len(dataX))
 			else:
-				self.saver.restore(sess, model)
-				print('Model %s restored' % model)
+				# If reached the end of the batch, reshuffle and start from beginning
+				random.shuffle(data_inds)
+				batch_start = 0
+				batch_end = batch_size
 
-			batch_start = 0
-			batch_end = batch_size
-
-			for step in range(1, num_steps+1):
-				batch_inds = data_inds[batch_start:batch_end]
-				batch_x = [dataX[i] for i in batch_inds]
-				batch_y = [dataY[i] for i in batch_inds]
-
-				# Increment next batch
-				if batch_end < len(dataX):
-					batch_start += batch_size
-					batch_end += batch_size
-					batch_end = min(batch_end,len(dataX))
-				else:
-					# If reached the end of the batch, reshuffle and start from beginning
-					random.shuffle(data_inds)
-					batch_start = 0
-					batch_end = batch_size
-
-				# Run optimization op (backprop)
-				loss, _ = sess.run([self.loss_op, self.optimizer],\
-					feed_dict={self.X: batch_x, self.Y:batch_x})
-				if step % display_step == 0 or step == 1:
-					# Calculate batch loss and accuracy
-					print("Step " + str(step) + ", Minibatch Loss= " + "{:.4f}".format(loss))
-
-			self.save_path = self.saver.save(sess, model)
-			print("Model saved in file: %s" % self.save_path)
-
+			# Run optimization op (backprop)
+			loss, _ = self.sess.run([self.loss_op, self.optimizer],\
+				feed_dict={self.X: batch_x, self.Y: batch_y,\
+				self.learning_rate_placeholder: learning_rate})
+			if step % display_step == 0:
+				# Calculate batch loss and accuracy
+				print("Step " + str(step) + ", Minibatch Loss= " + "{:.4f}".format(loss))
 
 # For testing
-	def test(self, dataX, dataY, model=None):
-		self.init_sess
+	def test(self, dataX, dataY):
+		loss = 0
 
-		with tf.Session() as sess:
-			self.saver.restore(sess, model)
-			print('Model %s restored' % model)
-
-			# Testing
-			for i in range(len(dataX)):
-				nn_output, loss = sess.run([self.output, self.loss_op],\
-					feed_dict={self.X: [dataX[i]], self.Y: [dataY[i]]})
-				print(nn_output)
-				print(dataY[i])
-				#print(loss)
+		# Testing
+		nn_output, loss = self.sess.run([self.output, self.loss_op],\
+			feed_dict={self.X: dataX, self.Y: dataY})
+		#print(dataY)
+		#print(nn_output)
+		return loss
 
 # For forward runs
-	def openSession(self, model=None):
-		self.init_sess()
-		self.sess = tf.Session()
-		self.saver.restore(self.sess, model)
-
-	def closeSession(self):
-		self.sess.close()
-
 	def run(self, dataX):
 		nn_output = self.sess.run(self.output, feed_dict={self.X: dataX})
 		return nn_output
--- myrobot.py
+++ myrobot.py
@@ -4,7 +4,7 @@ from numpy import cos, sin
 
 import random
 
-from FFNN import FFNN_Regression
+from FFNN import FFNN_1
 
 class link():
 	def __init__(self, tp, a, alpha, d, theta):
@@ -57,8 +57,10 @@ class myRobot():
 
 
 class myRRrobot(myRobot):
-	def __init__(self, link1len, link2len, elbow_dir):
+	def __init__(self, link1len, link2len, elbow_dir, invmodel, compmodel):
 		self.elbow_dir = elbow_dir # left or right
+		self.invmodel = invmodel
+		self.compmodel = compmodel
 
 		link1 = link('revolute', link1len, 0, 0, 0)
 		link2 = link('revolute', link2len, 0, 0, 0)
@@ -72,10 +74,10 @@ class myRRrobot(myRobot):
 		self.links[1].setconstraints(l2)
 
 	def setdefaultconstraints(self):
-		if self.elbow_dir == 'right':
-			self.setconstraints([0, np.pi/2], [0, np.pi/2])
-		elif self.elbow_dir == 'left':
-			self.setconstraints([np.pi/2, np.pi], [-np.pi/2, 0])
+		if self.elbow_dir.lower() == 'right':
+			self.setconstraints([0, np.pi/2], [0, np.pi])
+		elif self.elbow_dir.lower() == 'left':
+			self.setconstraints([np.pi/2, np.pi], [-np.pi, 0])
 		else:
 			print('What is this elbow direction?')
 			print(self.elbow_dir)
@@ -88,40 +90,159 @@ class myRRrobot(myRobot):
 			Qs.append(np.array([q1,q2]))
 		return Qs
 
-	def init_inverseNN(self):
-		self.ffnn = FFNN_Regression(hidden_layers=[128], num_inputs=2, num_outputs=2)
+	def randgenXs(self, numXs):
+		Xs = []
+		for n in range(numXs):
+			q1 = random.random()*self.links[0].c_range + self.links[0].c_span[0]
+			q2 = random.random()*self.links[1].c_range + self.links[1].c_span[0]
+			Xs.append(self.forward([q1,q2])[0:2,3])
+		return Xs
 
+
+#### INVERSE MODEL NEURAL NETWORK (BRAIN STEM) ###
+# NN session open and closes
+	def init_inverseNN(self):
+		self.inv_ffnn = FFNN_1(hidden_layers=[128], num_inputs=2,\
+			num_outputs=2, model=self.invmodel)
+
+	def open_inverseNN_session(self):
+		# None is default
+		self.inv_ffnn.openSession()
+
+	def close_inverseNN_session(self):
+		self.inv_ffnn.closeSession()
+
+# NN training
 	def train_inverseNN(self, num_points=100000, learning_rate=0.001, \
-		num_steps=5000, batch_size=128,\
-		display_step=100, model='./models/invkinRR.ckpt'):
-		
+		num_steps=100000, batch_size=128,\
+		display_step=100):
+
 		Qs = self.randgenQs(num_points)
 		Xs = np.array([self.forward(q)[0:2,3] for q in Qs])
-		self.ffnn.init_optimizer(learning_rate)
-		self.ffnn.train(Xs, Qs, num_steps, batch_size, display_step, model)
+		self.inv_ffnn.train(Xs, Qs, learning_rate, num_steps, batch_size, display_step)
 
-	def test_inverseNN(self, Xs, Qs, model='./models/invkinRR.ckpt'):
-		self.ffnn.test(Xs, Qs, model)
+	def save_inverseNN(self):
+		self.inv_ffnn.saveNN()
 
-	def load_inverseNN(self, model='./models/invkinRR.ckpt'):
-		# None is default
-		self.ffnn.openSession(model)
+# NN testing
+	def test_inverseNN(self, num_points=10):
+		Qs = self.randgenQs(num_points)
+		Xs = np.array([self.forward(q)[0:2,3] for q in Qs])
+		print("Test Xs:")
+		print(Xs)
+		loss = self.inv_ffnn.test(Xs, Qs)
+		print(loss)
 
-	def close_inverseNN(self):
-		self.ffnn.closeSession()
+## NN inverse model
+	def inverse(self, Xs):
+		return self.inv_ffnn.run(Xs)
 
-	def inverse(self, X):
-		return self.ffnn.run([X])
+	def inverseSingle(self, X):
+		Q = self.inverse([X])
+		return Q[0]
 
-	def inverseBatch(self, Xs, model='./models/invkinRR.ckpt'):
-		self.load_inverseNN(model)
-		nnQs = [self.inverse(X) for X in Xs]
-		self.close_inverseNN()
+	def inverseBatch(self, Xs):
+		nnQs = self.inverse(Xs)
 		return nnQs
 
 
+### COMPENSATION NEURAL NETWORK (CEREBELLUM) ###
+# NN session open and closes
+	def init_compNN(self):
+		self.comp_ffnn = FFNN_1(hidden_layers=[128], num_inputs=2,\
+			num_outputs=2, model=self.compmodel)
+
+	def open_compNN_session(self):
+		# None is default
+		self.comp_ffnn.openSession()
+
+	def close_compNN_session(self):
+		self.comp_ffnn.closeSession()
+
+# compensator NN initial training
+	def train_compNN(self, num_points=100000, learning_rate=0.001, \
+		num_steps=100000, batch_size=128,\
+		display_step=100):
+
+		Xs = self.randgenXs(num_points)
+		Qs = self.inverseBatch(Xs)
+		Ys = np.array([self.forward(q)[0:2,3] for q in Qs])
+		Ds = Ys-Xs
+		self.comp_ffnn.train(Qs, Ds, learning_rate, num_steps, batch_size, display_step)
+
+	def save_compNN(self):
+		self.comp_ffnn.saveNN()
+
+# compensator NN testing
+	def test_inverseNN(self, num_points=10):
+		Xs = self.randgenXs(num_points)
+		Qs = self.inverseBatch(Xs)
+		Ys = np.array([self.forward(q)[0:2,3] for q in Qs])
+		Ds = Ys-Xs
+		print("Test Qs:")
+		print(Qs)
+		loss = self.comp_ffnn.test(Qs, Ds)
+		print(loss)
+
+## compensator NN forward run (single input only)
+	def compensate(self, Q):
+		D = self.comp_ffnn.run([Q])
+		return D[0]
+
+
+### Full loop for reaching, with learning ###
+	def reach(self, X, learn=True, learning_rate=0.01, num_steps=10, display_step=100):
+		Q = self.inverseSingle(X)
+		D = self.compensate(Q)
+		Q_comp = self.inverseSingle(X-D)
+		Y = self.forward(Q_comp)[0:2,3]
+		D_comp = Y-X
+
+		if learn:
+			self.comp_ffnn.train([Q], [D_comp], learning_rate, num_steps, 1, display_step)
+
+		return Y
+
 
 if __name__ == "__main__":
-	myRR = myRRrobot(50,50,'right')
+	myRR = myRRrobot(50,50,'right',\
+		'./models/invRR_right/invkinRR_right.ckpt',\
+		'./models/compRR_right/compensationRR_right.ckpt')
 	myRR.init_inverseNN()
-	myRR.train_inverseNN()
+	myRR.open_inverseNN_session()
+	#myRR.train_inverseNN(display_step=1000)
+	#myRR.save_inverseNN()
+	#myRR.test_inverseNN(10)
+
+	myRR.init_compNN()
+	myRR.open_compNN_session()
+	#myRR.train_compNN(display_step=1000)
+	#myRR.save_compNN()
+
+	x = [30,60]
+	noloop = myRR.forward(myRR.inverseSingle(x))[0:2,3]
+	withloop = myRR.reach(x, learn=False)
+
+	print(noloop)
+	print(withloop)
+
+	# learn a few times
+	for i in range(1000):
+		withloop = myRR.reach(x, learn=True, learning_rate=0.005)
+		if i%100 == 0:
+			print('Reach %d: %s' % (i, str(withloop)))
+
+	print(myRR.reach(x, learn=False))
+
+	# q = myRR.inverseSingle([10,80])
+	# print(q)
+	# y = myRR.forward(q)[0:2,3]
+
+	# print(y)
+	# qs = myRR.inverseBatch([[1,1],[2,2]])
+	# print(qs)
+
+	myRR.save_inverseNN()
+	myRR.save_compNN()
+	myRR.close_compNN_session()
+	myRR.close_inverseNN_session()
